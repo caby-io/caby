0a336892549fdd529898c7aa4e685b1504ccdf21
Suhaib Malik <scrolltro0l@gmail.com>
2026-03-28 14:43:56 -0500

    chore: make spaces selector prettier

--- caby-web/src/routes/(app)/files/[space]/[...path]/SpacesSelector.svelte
+++ caby-web/src/routes/(app)/files/[space]/[...path]/SpacesSelector.svelte
@@ -1,21 +1,19 @@
 <script lang="ts">
 	import type { Space } from '$lib/space';
 
-	let {
-		current_space,
-		spaces
-	}: { current_space: Space | undefined; spaces: Space[] } = $props();
+	let { current_space, spaces }: { current_space: Space | undefined; spaces: Space[] } = $props();
 
 	let popover: HTMLDivElement;
 </script>
 
-<button popovertarget="space-selector-menu" class="fx fx--cc button spaces-button">
-	{current_space?.display ?? current_space?.name ?? ''}
+<button popovertarget="space-selector-menu" class="fx button spaces-button">
+	<span class="fx-grow">{current_space?.display ?? current_space?.name ?? ''}</span>
+	<iconify-icon icon="lucide:chevron-left" class="caret"></iconify-icon>
 </button>
 
 <div bind:this={popover} id="space-selector-menu" popover>
 	{#each spaces as space}
-		<a class="fx fx--cc button" href="/files/{space.name}" onclick={() => popover.hidePopover()}
+		<a class="button" href="/files/{space.name}" onclick={() => popover.hidePopover()}
 			>{space.display}</a
 		>
 	{/each}
@@ -25,6 +23,15 @@
 	.spaces-button {
 		margin: 1rem;
 		anchor-name: --spaces-button;
+		align-items: center;
+
+		.caret {
+			transition: transform 0.2s;
+		}
+	}
+
+	.spaces-button:has(+ #space-selector-menu:popover-open) .caret {
+		transform: rotate(-90deg);
 	}
 
 	#space-selector-menu {
@@ -37,9 +44,10 @@
 		left: anchor(left);
 		margin-block-start: 0.25rem;
 		font-weight: normal;
+		box-shadow: var(--box-shadow-0);
 
 		border: 0;
-		border-radius: 5px;
+		// border-radius: 5px;
 		background: var(--clr-background-2);
 
 		&:popover-open {
@@ -47,8 +55,17 @@
 			flex-direction: column;
 		}
 
-		> button {
+		> .button {
 			border-radius: 0;
+			text-decoration: none;
+
+			&:first-of-type {
+				border-radius: 5px 5px 0 0;
+			}
+
+			&:last-of-type {
+				border-radius: 0 0 5px 5px;
+			}
 		}
 	}
 </style>
