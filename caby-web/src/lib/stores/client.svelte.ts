dd008d6efbd1fc25840377fe76c0f549b8c383eb
Suhaib Malik <scrolltro0l@gmail.com>
2026-04-11 17:46:37 -0500

    feat: get api base from env var

--- caby-web/src/lib/stores/client.svelte.ts
+++ caby-web/src/lib/stores/client.svelte.ts
@@ -1,3 +1,4 @@
+import { PUBLIC_API_BASE } from '$env/static/public';
 import { ApiClient } from '$lib/api/client';
 
-export const client = $state(new ApiClient('http://localhost:8080/v0'));
+export const client = $state(new ApiClient(PUBLIC_API_BASE));
