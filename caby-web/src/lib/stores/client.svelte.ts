728e3d288d31dc112f638272cc96433d45aef78a
Suhaib Malik <scrolltro0l@gmail.com>
2026-04-18 21:07:16 -0500

    chore: add docker images and gitlab ci for internal registry

--- caby-web/src/lib/stores/client.svelte.ts
+++ caby-web/src/lib/stores/client.svelte.ts
@@ -1,4 +1,4 @@
 import { env } from '$env/dynamic/public';
 import { ApiClient } from '$lib/api/client';
 
-export const client = $state(new ApiClient({ api_base: env.PUBLIC_API_BASE ?? 'http://localhost:8080' }));
+export const client = $state(new ApiClient({ api_base: env.PUBLIC_API_BASE ?? 'http://localhost:8080/v0' }));
