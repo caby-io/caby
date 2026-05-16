90a94ba138d5114ea4673b677ac7797ff0dde843
Suhaib Malik <scrolltro0l@gmail.com>
2026-05-16 16:50:55 -0500

    style: make files left menu a drawer on mobile
    style: add starter media queries to make things usable on mobile

--- caby-web/svelte.config.js
+++ caby-web/svelte.config.js
@@ -1,11 +1,21 @@
 import adapter from '@sveltejs/adapter-node';
 import { sveltePreprocess } from 'svelte-preprocess';
+import { fileURLToPath } from 'node:url';
+
+const libDir = fileURLToPath(new URL('./src/lib', import.meta.url));
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: sveltePreprocess(),
+	preprocess: sveltePreprocess({
+		scss: {
+			importer: (url) => {
+				if (!url.startsWith('$lib/')) return null;
+				return { file: `${libDir}/${url.slice('$lib/'.length)}` };
+			}
+		}
+	}),
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
