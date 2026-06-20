1da5a05fef1e897860b9d86e4f2ed7a3854b5c7a
Suhaib Malik <scrolltro0l@gmail.com>
2026-06-20 13:51:18 -0500

    feat: add frontend thumbnail display

--- caby-web/src/routes/(app)/files/[space]/[...path]/File.svelte
+++ caby-web/src/routes/(app)/files/[space]/[...path]/File.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
-	import { join } from '$lib/fs';
 	import type { EntryProps, FileFields } from '$lib/files/entry';
 	import IconFlatColorIconsFile from '~icons/flat-color-icons/file';
+	import IconFlatColorIconsImageFile from '~icons/flat-color-icons/image-file';
+	import IconFlatColorIconsVideoFile from '~icons/flat-color-icons/video-file';
+	import IconFlatColorIconsAudioFile from '~icons/flat-color-icons/audio-file';
+	import IconFlatColorIconsDocument from '~icons/flat-color-icons/document';
+	import IconFlatColorIconsPackage from '~icons/flat-color-icons/package';
 
 	let {
 		entry,
@@ -15,9 +19,33 @@
 		onContextMenu
 	}: EntryProps<FileFields> = $props();
 
+	function pickKindIcon(kind: string) {
+		switch (kind) {
+			case 'image':
+				return IconFlatColorIconsImageFile;
+			case 'video':
+				return IconFlatColorIconsVideoFile;
+			case 'audio':
+				return IconFlatColorIconsAudioFile;
+			case 'pdf':
+			case 'document':
+				return IconFlatColorIconsDocument;
+			case 'archive':
+				return IconFlatColorIconsPackage;
+			default:
+				return IconFlatColorIconsFile;
+		}
+	}
+
 	let is_selected = $derived(entry.is_selected);
 	let is_processing = $derived(entry.is_processing);
 	let is_targetted = $derived(entry.is_targetted);
+
+	let img_failed = $state(false);
+	let kind = $derived(entry.entry_fields.kind);
+	let preview_url = $derived(entry.entry_fields.preview_url);
+	let show_preview = $derived(kind === 'image' && !!preview_url && !img_failed);
+	let KindIcon = $derived(pickKindIcon(kind));
 </script>
 
 <div
@@ -34,7 +62,13 @@
 	ondrop={(e) => onDrop!(e, entry)}
 	oncontextmenu={(e) => onContextMenu!(e, entry)}
 >
-	<section class="display fx fx--cc fx-grow"><IconFlatColorIconsFile /></section>
+	{#if show_preview}
+		<section class="display fx fx--cc fx-grow">
+			<img src={preview_url} alt={entry.name} loading="lazy" onerror={() => (img_failed = true)} />
+		</section>
+	{:else}
+		<section class="display fx fx--cc fx-grow"><KindIcon /></section>
+	{/if}
 	<section class="info">
 		<!-- todo: consider splitting extension so we can show it-->
 		<h1 title={entry.name}>{entry.name}</h1>
