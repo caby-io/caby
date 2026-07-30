098491759c338c85fefea037c350d2ed7c37b268
Suhaib Malik <scrolltro0l@gmail.com>
2026-07-30 11:14:41 -0500

    chore: make auth extractor var consistent

--- caby-service/src/web/auth_api/auth_logout.rs
+++ caby-service/src/web/auth_api/auth_logout.rs
@@ -14,17 +14,15 @@ pub struct LogoutResponse<'a> {
     user: &'a str,
 }
 
-pub async fn handle_logout(State(_cfg): State<Config>, authorized_user: AuthUser) -> Response {
-    let Some(account) = authorized_user.as_account() else {
+pub async fn handle_logout(State(_cfg): State<Config>, auth: AuthUser) -> Response {
+    let Some(account) = auth.as_account() else {
         return jsend::JSendBuilder::new()
             .status_code(StatusCode::FORBIDDEN)
             .fail("only account users can log out")
             .into_response();
     };
 
-    let session_path = account
-        .path
-        .join(format!("session_{}", authorized_user.token));
+    let session_path = account.path.join(format!("session_{}", auth.token));
 
     if let Err(err) = fs::remove_file(&session_path).await {
         error!("could not delete session file: {:#}", err);
